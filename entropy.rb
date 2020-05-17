73735e61de2df970bb199efe3f026d5c15cd535f
Bryce Mercines <nplixel@gmail.com>
2020-05-17 15:39:50 +0800

    Changes to be committed:   modified:   entropy.rb
    - completed sending report functionality
    - randomly generated coordinates with boundingbox for the philippines
    - need to improve coordnated generation
    - need to apply cancellation interface

--- entropy.rb
+++ entropy.rb
@@ -2,15 +2,16 @@ require 'rubygems'
 require 'socket.io-client-simple'
 require 'json'
 
-API_URL = "http://127.0.0.1:7000"
+API_URL = "https://sagipinasV1.herokuapp.com"
 
 
 socket = SocketIO::Client::Simple.connect API_URL
 
 #!/usr/bin/env ruby
 
-
 def sendReport(type, count, socket)
+
+ incidentTypes = ['earthquake','fire','flooding','accident','landslide']
 
   reportData = {
     :id => "2695506260470251",
@@ -18,28 +19,28 @@ def sendReport(type, count, socket)
     :specified => "",
     :details => "This just a test using entropy script",
     :location => {
-      :lat=> "14.509705",
-      :long=> " 121.001852"
+      :lat=> "#{rand(4..20)}.509705",
+      :long=> "#{rand(120..124)}.001852"
     },
-    :status => "cancelled",
+    :status => "unverfied",
     :timestamp => Time.now
   }
 
-  for i in 0..count.to_i
+  for i in 1..count.to_i
     sleep 0.3
+
+    if type == "rand"
+      reportData[:type] = incidentTypes[rand(0..incidentTypes.length()-1)]
+    end
+    puts "[SENT REPORT: #{reportData[:type]}] => #{Time.now}"
     socket.emit :test_report_data, reportData
   end
 end
 
 
+validArgTypes = ['earthquake','fire','flooding','accident','landslide','rand']
 
-# ARGV.each do|a|
-#   sleep 0.5
-#   sendData(a, socket)
-# end
-
-types = ['earthquake','fire','flooding','accident','landslide']
-
+puts "=============================="
 puts "Running Entropy test script..."
 puts "=============================="
 
@@ -47,7 +48,12 @@ type = ARGV[0];
 count = ARGV[1];
 
 if count != "inf"
-  sendReport(type,count,socket)
+  if validArgTypes.include? type
+    sendReport(type,count,socket)
+  else
+    puts "[FATAL ERROR]: unknown incident type provided: #{type}"
+    puts "Please type rand if you want a random incident value."
+  end
 else
   puts "Infinite Requests: not supported yet"
 end
