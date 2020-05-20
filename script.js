d942082011a5bf47f40e0ce85875a1d10a69e6b8
Bryce Mercines <nplixel@gmail.com>
2020-05-20 21:46:13 +0800

    added scripts file and changed name to index.html for UI

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+const coreURL = "https://sagipinasv1.herokuapp.com";
+const socket = io(coreURL);
+
+function cancelReport(id){
+  alert(id)
+  socket.emit("test_cancel",{id: id})
+}
+
+
+
+const dataRow = (count,data) => {
+  return (`
+ <tr>
+ <th scope="row">${count+1}</th>
+  <td>${data.type}</td>
+  <td>${data.details}</td>
+  <td>
+    <button class="btn btn-dark btn-sm" onclick="cancelReport('${data.uid}')">CANCEL</button>
+  </td>
+</tr>
+`);
+}
+
+const renderTable = () => {
+  document.getElementById('table-data').innerHTML = "";
+  axios.get(`${coreURL}/incidents`)
+  .then(res=> {
+     res.data.forEach((item,index)=>{
+       document.getElementById('table-data').innerHTML += dataRow(index,item)
+     })
+  })
+}
+
+renderTable()
+
+socket.on("report",()=> {
+  renderTable()
+})
+
+
